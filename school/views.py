4f6f384cd79f73ef3f015409de2dbef28fc6df4c
Наталья <Lisicina_natasha@bk.ru>
2021-12-21 08:57:38 +0400

    ORM_school

---
+++ school/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    students_object=Student.objects.all()
+
+    context = {'students_object': students_object}
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    ordering = 'group'
+
+    return render(request, template, context)
